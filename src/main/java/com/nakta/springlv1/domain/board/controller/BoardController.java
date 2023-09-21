ef793583b1d8139b3f75e8d98e6837a6ea4bb398
DESKTOP-HMN2L1J\정강용 <tuto3355@naver.com>
2023-09-21 21:25:32 +0900

    타임리프 테스트중

--- src/main/java/com/nakta/springlv1/domain/board/controller/BoardController.java
+++ src/main/java/com/nakta/springlv1/domain/board/controller/BoardController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
+@RestController//테스트
 @RequiredArgsConstructor
 @RequestMapping("/api/board")
 public class BoardController {
