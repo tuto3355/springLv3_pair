0e0890dbb76840155fa5cf3023c4a2c9848408ef
DESKTOP-HMN2L1J\정강용 <tuto3355@naver.com>
2023-09-24 01:52:07 +0900

    타임리프 테스트중

---
+++ src/main/java/com/nakta/springlv1/domain/board/controller/AsideController.java
@@ -0,0 +1,13 @@
+package com.nakta.springlv1.domain.board.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AsideController {
+    @GetMapping("/seller") // 상품관리페이지
+    public String showStore(Model model) {
+        return "store"; // 상품 관리 페이지로 이동
+    }
+}
--- src/main/java/com/nakta/springlv1/global/config/WebSecurityConfig.java
+++ src/main/java/com/nakta/springlv1/global/config/WebSecurityConfig.java
@@ -74,7 +74,8 @@ public class WebSecurityConfig {
                                 "/webjars/**",
                                 /* swagger v3 */
                                 "/v3/api-docs/**",
-                                "/swagger-ui/**").permitAll() // '/api/user/'로 시작하는 요청 모두 접근 허가
+                                "/swagger-ui/**",
+                                "/**").permitAll() // '/api/user/'로 시작하는 요청 모두 접근 허가
                         .anyRequest().authenticated()
 
         );
